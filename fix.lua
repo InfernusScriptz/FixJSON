af79999d8251ba6e31501e14987577b8d638fad0
InfernusScriptz <128822811+InfernusScriptz@users.noreply.github.com>
2023-07-10 22:46:05 +0300

    Update fix.lua

--- fix.lua
+++ fix.lua
@@ -1,4 +1,4 @@
 if not _G.fixedJSON then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Robbie-Wittenhagen/JSON/Main/JSON.lua')..'\n\nhookfunction(game.HttpService.JSONEncode, JSON["encode"])\nhookfunction(game.HttpService.JSONDecode, JSON["decode"])')()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/InfernusScriptz/FixJSON/main/newJSON.lua')..'\n\nhookfunction(game.HttpService.JSONEncode, JSON["encode"])\nhookfunction(game.HttpService.JSONDecode, JSON["decode"])')()
 	_G.fixedJSON = true
 end
